595039a15640edc431f3b957193e56075782439f
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2025-03-25 13:36:44 +0200

    made the location page responsive

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -7,8 +7,8 @@ export default function Location() {
 					{/* <Separator className="bg-black h-1 rounded-lg mt-2 w-auto" /> */}
 				</h2>
 
-				<div className="flex flex-row gap-8 !pt-0 md:pt-12 lg:flex-row">
-					<div className="flex w-[50%] flex-col gap-8">
+				<div className="flex flex-col gap-8 !pt-0 sm:flex-row md:pt-12 lg:flex-row">
+					<div className="flex w-full flex-col gap-8 sm:w-[50%]">
 						<p className="text-justify text-xl">
 							Събитието ще се проведе на Х ден от 9:30 в Ларго, пл. Независимост, София. До там може да се
 							стигне с частен самолет, хеликоптер, автомобил, велосипед или пеша.Събитието ще се проведе
@@ -20,7 +20,7 @@ export default function Location() {
 							им позволява да се позиционират възможно най-бързо в технологичния сектор.
 						</p>
 					</div>
-					<div className="border-gray-300 w-[50%] overflow-hidden rounded-lg border">
+					<div className="border-gray-300 w-full overflow-hidden rounded-lg border sm:w-[50%]">
 						<iframe
 							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d733.0729700320611!2d23.32159637614092!3d42.6975400791878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa856ee63f96cf%3A0xa6d65ce76c5bbe5f!2sSofia%20Center%2C%20pl.%20%22Nezavisimost%22%2C%201000%20Sofia!5e0!3m2!1sen!2sbg!4v1742839905608!5m2!1sen!2sbg"
 							width="100%"
