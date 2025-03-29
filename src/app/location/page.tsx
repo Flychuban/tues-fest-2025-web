d745a506ed50004264b0d18642c691d124962733
bvpav <78971592+bvpav@users.noreply.github.com>
2025-03-29 17:30:32 +0200

    added metadata for location page

--- src/app/location/page.tsx
+++ src/app/location/page.tsx
@@ -1,3 +1,16 @@
+import { Metadata } from 'next';
+
+const LOCATION_MAP_URL =
+	'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d733.0729700320611!2d23.32159637614092!3d42.6975400791878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa856ee63f96cf%3A0xa6d65ce76c5bbe5f!2sSofia%20Center%2C%20pl.%20%22Nezavisimost%22%2C%201000%20Sofia!5e0!3m2!1sen!2sbg!4v1742839905608!5m2!1sen!2sbg';
+
+const MOST_IMPORTANT_PARAGRAPH =
+	'Събитието ще се проведе на Х ден от 9:30 в Ларго, пл. Независимост, София. До там може да се стигне с частен самолет, хеликоптер, автомобил, велосипед или пеша.Събитието ще се проведе на Х ден от 9:30 в Ларго, пл. Независимост, София. До там може да се стигне с частен самолет, хеликоптер, автомобил, велосипед или пеша.';
+
+export const metadata = {
+	title: 'Локация',
+	description: MOST_IMPORTANT_PARAGRAPH,
+} satisfies Metadata;
+
 export default function Location() {
 	return (
 		<div className="m-auto min-h-screen max-w-screen-2xl p-8 !pt-28 md:p-12">
@@ -9,12 +22,7 @@ export default function Location() {
 
 				<div className="flex flex-col gap-8 !pt-0 sm:flex-row md:pt-12 lg:flex-row">
 					<div className="flex w-full flex-col gap-8 sm:w-[50%]">
-						<p className="text-justify text-xl">
-							Събитието ще се проведе на Х ден от 9:30 в Ларго, пл. Независимост, София. До там може да се
-							стигне с частен самолет, хеликоптер, автомобил, велосипед или пеша.Събитието ще се проведе
-							на Х ден от 9:30 в Ларго, пл. Независимост, София. До там може да се стигне с частен
-							самолет, хеликоптер, автомобил, велосипед или пеша.
-						</p>
+						<p className="text-justify text-xl">{MOST_IMPORTANT_PARAGRAPH}</p>
 						<p className="text-justify text-xl">
 							Възпитаниците на ТУЕС преминават през задълбочена и специализирана 5-годишна програма, която
 							им позволява да се позиционират възможно най-бързо в технологичния сектор.
@@ -22,7 +30,7 @@ export default function Location() {
 					</div>
 					<div className="border-gray-300 w-full overflow-hidden rounded-lg border sm:w-[50%]">
 						<iframe
-							src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d733.0729700320611!2d23.32159637614092!3d42.6975400791878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa856ee63f96cf%3A0xa6d65ce76c5bbe5f!2sSofia%20Center%2C%20pl.%20%22Nezavisimost%22%2C%201000%20Sofia!5e0!3m2!1sen!2sbg!4v1742839905608!5m2!1sen!2sbg"
+							src={LOCATION_MAP_URL}
 							width="100%"
 							height="400"
 							className="border-0"
