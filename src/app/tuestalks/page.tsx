aeccf0c8b7cb7191b6e32fd3a0dd367830478d8e
bvpav <78971592+bvpav@users.noreply.github.com>
2025-04-02 01:39:51 +0300

    added animation to tt page

--- src/app/tuestalks/page.tsx
+++ src/app/tuestalks/page.tsx
@@ -1,7 +1,20 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'TUES Talks - първият ученически подкаст',
+	description:
+		'TUES Talks е първият ученически подкаст, воден от ученици за ученици от ТУЕС. Подкастът дискутира технологии, книги, спорт и много други теми, свързани с ТУЕС и извън него.',
+	openGraph: {
+		title: 'TUES Talks - първият ученически подкаст',
+		description:
+			'TUES Talks е първият ученически подкаст, воден от ученици за ученици от ТУЕС. Подкастът дискутира технологии, книги, спорт и много други теми, свързани с ТУЕС и извън него.',
+	},
+};
+
 const TUESTalksPage = () => (
 	<div className="flex h-[calc(100vh-var(--header-height)-var(--footer-height))] items-center justify-center p-8 md:p-12">
 		<iframe
-			className="w-[90%] max-w-[960px] overflow-hidden rounded-xl sm:w-[60%]"
+			className="animate-in fade-in w-[90%] max-w-[960px] overflow-hidden rounded-xl duration-1000 sm:w-[60%]"
 			src="https://open.spotify.com/embed/show/2HGIZmqHAFtNwfXOrlTu7v?utm_source=generator"
 			height="352"
 			sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
