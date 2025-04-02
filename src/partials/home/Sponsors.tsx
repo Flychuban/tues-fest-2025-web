0982c97b8e31991d9df6bc85c2d4f1b08f237f6d
bvpav <78971592+bvpav@users.noreply.github.com>
2025-04-02 23:48:11 +0300

    attempt to fix caching error

--- src/partials/home/Sponsors.tsx
+++ src/partials/home/Sponsors.tsx
@@ -9,28 +9,43 @@ import {
 } from '@/constants/home/sponsors';
 import PodkrepqAutoDisplay from './sponsors/PodkrepqAutoDisplay';
 
-function randomStartIndex(podkrepqshti: Podkrepqsht[]) {
+async function randomStartIndex(podkrepqshti: Podkrepqsht[]) {
 	return Math.floor(Math.random() * podkrepqshti.length);
 }
 
-const Sponsors = () => (
-	<section id="sponsors" className="relative z-20 mb-20 p-8 md:p-12">
-		<div className="py-4 pb-8">
-			<GradientHeading size="lg">Спонсори</GradientHeading>
-		</div>
-		<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">Алфа Спонсори</h2>
-		<PodkrepqAutoDisplay podkrepqshti={ALPHA_SPONSORS} startIndex={randomStartIndex(ALPHA_SPONSORS)} />
-		<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">Бета Спонсори</h2>
-		<PodkrepqAutoDisplay podkrepqshti={BETA_SPONSORS} startIndex={randomStartIndex(BETA_SPONSORS)} />
-		<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">Гама Спонсори</h2>
-		<PodkrepqAutoDisplay podkrepqshti={GAMMA_SPONSORS} startIndex={randomStartIndex(GAMMA_SPONSORS)} />
-		<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">Партньори</h2>
-		<PodkrepqAutoDisplay podkrepqshti={PARTNERS} startIndex={randomStartIndex(PARTNERS)} />
-		<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">
-			Медийни Партньори
-		</h2>
-		<PodkrepqAutoDisplay podkrepqshti={MEDIA_PARTNERS} startIndex={randomStartIndex(MEDIA_PARTNERS)} />
-	</section>
-);
+export default async function Sponsors() {
+	const [alphaStartIndex, betaStartIndex, gammaStartIndex, partnersStartIndex, mediaPartnersStartIndex] =
+		await Promise.all([
+			randomStartIndex(ALPHA_SPONSORS),
+			randomStartIndex(BETA_SPONSORS),
+			randomStartIndex(GAMMA_SPONSORS),
+			randomStartIndex(PARTNERS),
+			randomStartIndex(MEDIA_PARTNERS),
+		]);
 
-export default Sponsors;
+	return (
+		<section id="sponsors" className="relative z-20 mb-20 p-8 md:p-12">
+			<div className="py-4 pb-8">
+				<GradientHeading size="lg">Спонсори</GradientHeading>
+			</div>
+			<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">
+				Алфа Спонсори
+			</h2>
+			<PodkrepqAutoDisplay podkrepqshti={ALPHA_SPONSORS} startIndex={alphaStartIndex} />
+			<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">
+				Бета Спонсори
+			</h2>
+			<PodkrepqAutoDisplay podkrepqshti={BETA_SPONSORS} startIndex={betaStartIndex} />
+			<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">
+				Гама Спонсори
+			</h2>
+			<PodkrepqAutoDisplay podkrepqshti={GAMMA_SPONSORS} startIndex={gammaStartIndex} />
+			<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">Партньори</h2>
+			<PodkrepqAutoDisplay podkrepqshti={PARTNERS} startIndex={partnersStartIndex} />
+			<h2 className="text-accent-foreground font-title mb-4 mt-32 text-center text-3xl font-black">
+				Медийни Партньори
+			</h2>
+			<PodkrepqAutoDisplay podkrepqshti={MEDIA_PARTNERS} startIndex={mediaPartnersStartIndex} />
+		</section>
+	);
+}
