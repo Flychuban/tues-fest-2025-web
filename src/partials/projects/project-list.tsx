7dec7b9ce69c4865c016ff0fcefb39d7efd42c39
bvpav <78971592+bvpav@users.noreply.github.com>
2025-04-18 20:26:13 +0300

    added search empty state

--- src/partials/projects/project-list.tsx
+++ src/partials/projects/project-list.tsx
@@ -3,13 +3,15 @@
 import { useMemo } from 'react';
 import { useQueryState } from 'nuqs';
 
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import { ProjectType } from '@/app/projects/actions';
 import { filterItems } from '@/lib/filter';
 import { useDebounce } from '@/lib/hooks/use-debounce';
 import { ProjectCard } from './project/project-card';
 
 export function ProjectList({ projects }: { projects: ProjectType[] }) {
-	const [search] = useQueryState('search');
+	const [search, setSearch] = useQueryState('search');
 	const debouncedSearch = useDebounce(search, 300);
 
 	const filteredProjects = useMemo(
@@ -23,6 +25,22 @@ export function ProjectList({ projects }: { projects: ProjectType[] }) {
 				: projects,
 		[debouncedSearch, projects]
 	);
+
+	if (filteredProjects.length === 0) {
+		return (
+			<Card className="p-10 text-center">
+				<h3 className="text-2xl font-bold">Няма намерени проекти</h3>
+				<p className="text-muted-foreground text-lg">
+					Не успяхме да намерим проекти, съответстващи на вашето търсене.
+				</p>
+				{debouncedSearch && (
+					<Button onClick={() => setSearch(null)} variant="outline" className="mx-auto w-fit">
+						Изчисти търсенето
+					</Button>
+				)}
+			</Card>
+		);
+	}
 
 	return (
 		<div className="inline-grid w-full grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
